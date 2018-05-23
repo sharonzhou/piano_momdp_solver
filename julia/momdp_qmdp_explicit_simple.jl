57c00b71ec4762e873dfe9f5fa17db88219782e7
Sharon Zhou <sharonzhou@DN525b70.SUNet>
2018-05-22 23:06:49 -0700

    math tricks

--- julia/momdp_qmdp_explicit_simple.jl
+++ julia/momdp_qmdp_explicit_simple.jl
@@ -238,7 +238,9 @@ tree = [[2,3,4,5]]
 node_idx = 6
 for node in tree
     # depth d requires # nodes = 2*(4^0+4^1+4^2+4^3+...4^d) (mult by 2 b/c it's num expansion nodes + num extra action nodes)
-    if node_idx < 3.0744573e+18 # d=30 is 3.0744573e+18 
+    # d=30 is 3.0744573e+18 (copy paste below for chrome calculator evaluation)
+    # 2*(4^0+4^1+4^2+4^4+4^5+4^6+4^7+4^8+4^9+4^10+4^11+4^12+4^13+4^14+4^15+4^16+4^17+4^18+4^19+4^20+4^21+4^22+4^23+4^24+4^25+4^26+4^27+4^28+4^29+4^30)=
+    if node_idx < 3.0744573e+18 
         if size(node, 1) == 1
             # add a 4 child next
             push!(tree, [node_idx, node_idx+1, node_idx+2, node_idx+3])
